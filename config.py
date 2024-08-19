3cf3ca1e305cf4280bbe755ef0507b355a5a96d6
Sergey Melnikov <tembelis@bk.ru>
2024-08-19 16:15:36 +0300

    Правки опции context и названия приложения

--- config.py
+++ config.py
@@ -8,14 +8,7 @@ from fatsecret_tests_project.utils import file_path
 def to_driver_options(context):
     options = UiAutomator2Options()
 
-    if context == "local_emulator":
-        options.set_capability("remote_url", os.getenv("REMOTE_URL"))
-        options.set_capability("deviceName", os.getenv("DEVICE_NAME"))
-        options.set_capability("appWaitActivity", os.getenv("APP_WAIT_ACTIVITY"))
-        options.set_capability("app", file_path.relative_path(os.getenv("APP")))
-        options.set_capability("appium:autoGrantPermissions", "true")
-
-    if context == "local_real_device":
+    if context == "local_emulator" or context == "local_real_device":
         options.set_capability("remote_url", os.getenv("REMOTE_URL"))
         options.set_capability("deviceName", os.getenv("DEVICE_NAME"))
         options.set_capability("appWaitActivity", os.getenv("APP_WAIT_ACTIVITY"))
